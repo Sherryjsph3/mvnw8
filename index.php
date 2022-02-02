f5b6758d0075eaf3d8e3d45974e56e47c599a6ce
Sherry Joseph <sherryjoseph@Sherrys-MacBook-Air.local>
2022-02-01 22:46:37 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>MVNW8</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="keywords" content="one on one coaching, nutrition, availabilty">
+        <meta name="description" content="MVNW8 Helps People Get Fit and Stay Fit.">
+        <meta name="robots" content="index"/>  
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+        <!-- Fonts --> <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+        <!-- jQuery --><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- Bootstrap CSS 5.1 --><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Bootstrap JS 5.1 --><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        </head>
+        <style>
+
+            html, body {
+                overflow-x: hidden;
+            }
+
+            .container {
+                padding: 0;
+                margin: 0;
+                width: 100%;
+                font-weight: normal;
+                font-stretch: normal;
+                font-style: normal;
+                line-height: normal;
+                letter-spacing: normal;
+            }
+
+            .row {
+                width: 100%;
+                margin: 0 auto !important;
+            }
+            
+            .navbar {
+                background-color: black;
+                height: 20px;
+                position: fixed;
+                width: 100%;
+                z-index: 999;
+                padding: 30px 0;
+            }
+            .header-row {
+                background: url("assets/gym-hero.png") no-repeat center;
+                background-size: 100% 100%;
+                height: 500px;
+                width:100%;
+                position: absolute;
+            }
+            .logo {
+                height: 40px;
+                margin: -50px auto;
+                text-align: center;
+                display: inline-block;
+                vertical-align: middle;
+                cursor: pointer;
+            }
+            .header-title {
+                text-align: center;
+                font-size: 50px;
+                color: rgb(92, 180, 249);
+                font-family: 'Bebas Neue';
+                letter-spacing: 0.6px;
+                font-weight: 600;
+                position: absolute;
+                z-index: 1;
+            }
+            .welcome {
+                width: 100%;
+                height: 450px;
+                background-color: rgb(0, 0, 0, 0.2);
+            }
+
+        </style>
+      
+        <body>
+            <!-- <div class="container"> -->
+                <!-- navigation -->
+                <div class="navbar">
+                        <img src="assets/logo.png" alt="mvnw8 logo" class="logo" >
+                </div>
+                <!-- header-->
+                <div class="header-row row">  
+                    <div class="header-title">Fitness Coaching That Works</div>
+                </div>
+                <!-- welcome -->
+                <div class="welcome">
+
+                </div>
+
+            <!-- </div> -->
+        </body>
+        </html>
+      
+
+
+
